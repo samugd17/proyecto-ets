33616ec3dcbcb7a70ee76d2b0a44d218da17d9f6
Samugd17 <samugd17@gmail.com>
2023-06-12 16:00:11 +0100

    Se reordena estructura

---
+++ source/tablestars.py
@@ -0,0 +1,58 @@
+import random
+
+current_users = {}
+
+def create_guest_name():
+    while True:
+        id_guest = random.randint(10000000, 99999999)
+        if id_guest not in current_users:
+            guest_name = f"Guest{id}"
+            current_users[guest_name] = 'placeholder'
+            return guest_name
+
+class User:
+    def __init__(self):
+        self.id = random.randint(10000000, 99999999)
+
+    def add_guest(self):
+        Guest()
+
+    def create_account(self, username, password):
+        if username not in current_users:
+            current_users[username] = password
+            return Member(username, password)
+        else:
+            return f"{username} ya está en uso"
+
+
+class Guest(User):
+    def __init__(self):
+        self.username = create_guest_name()
+
+
+class Member(User):
+    def __init__(self, username: str, password: str) -> None:
+        self.username = username
+        self.password = password
+        self.login_status = False
+
+    def login(self, username: str, password: str) -> None:
+        if username in current_users and current_users[username] == password:
+            self.login_status = True
+
+    def logout(self) -> None:
+        if self.login_status:
+            self.login_status = False
+
+
+class Inventory:
+    def __init__(self, username: str):
+        self.username = username
+        self.items:list = []
+
+    def add_item(self, *items: str) -> None:
+        for item in items:
+            self.items.append(item)
+
+    def remove_item(self, item: str) -> None:
+        self.items.remove(item)
